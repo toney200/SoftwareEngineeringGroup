abf6fe13139072813184e22e41dbfab2cdbc57d2
Eoin <116293113+DevEoin@users.noreply.github.com>
2024-10-15 14:44:07 +0100

    JUnit 5 Upgrade

--- Java Project/Newsagent2024/src/CustomerTest.java
+++ Java Project/Newsagent2024/src/CustomerTest.java
@@ -1,9 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.Test;
-
-//ffail
-
-import junit.framework.Assert;
+import  org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AssertionsKt;
 
 public class CustomerTest extends Customer{
     Customer testCust = new Customer();
@@ -15,7 +15,7 @@ public class CustomerTest extends Customer{
      * Should return false when a name is more than 20
      */
     public void testValidateFirstName001(){
-        Assert.assertEquals(false, testCust.validateFirstName("fnthrdsfytunmightjkiw"));
+        assertEquals(false, testCust.validateFirstName("fnthrdsfytunmightjkiw"));
     }
 
     @SuppressWarnings("deprecation")
@@ -25,7 +25,7 @@ public class CustomerTest extends Customer{
      * Should return false when first name less than 2
      */
     public void testValidateFirsName002(){
-        Assert.assertEquals(false, testCust.validateFirstName("E"));
+        assertEquals(false, testCust.validateFirstName("E"));
     }
 
     @SuppressWarnings("deprecation")
@@ -35,7 +35,7 @@ public class CustomerTest extends Customer{
      * Should return true when first name is 2 - 20(inclusive) characters and contains no numbers
      */
     public void testValidateFirstName003(){
-        Assert.assertEquals(true, testCust.validateFirstName("John"));
+        assertEquals(true, testCust.validateFirstName("John"));
     }
 
     @SuppressWarnings("deprecation")
@@ -45,7 +45,7 @@ public class CustomerTest extends Customer{
      * Should return false when first name contains numbers
      */
     public void testValidateFirstName004(){
-        Assert.assertEquals(false, testCust.validateFirstName("S4oirse"));
+        assertEquals(false, testCust.validateFirstName("S4oirse"));
     }
 
     @SuppressWarnings("deprecation")
@@ -55,7 +55,7 @@ public class CustomerTest extends Customer{
      * Should return false when last name is greater than 20 characters
      */
     public void testValidateLastName(){
-        Assert.fail("Test fails");
+        fail("Test fails");
     }
 
     @SuppressWarnings("deprecation")
@@ -65,7 +65,7 @@ public class CustomerTest extends Customer{
      * Should return false when the last name is shorter than 2 characters 
      */
     public void testValidateLastName002(){
-        Assert.fail();
+        fail();
     }
 
     @SuppressWarnings("deprecation")
@@ -75,7 +75,7 @@ public class CustomerTest extends Customer{
      * Should return true when valid name is entered 
      */
     public void testValidateLastName003(){
-        Assert.assertTrue(testCust.validateLastName("Murphy"));
+        assertTrue(testCust.validateLastName("Murphy"));
     }
 
     @SuppressWarnings("deprecation")
@@ -85,7 +85,25 @@ public class CustomerTest extends Customer{
      * Should return false when the name contains a digit.
      */
     public void testValidateLastName004(){
-        Assert.assertEquals(false, testCust.validateLastName("K3nndedy"));
+        assertEquals(false, testCust.validateLastName("K3nndedy"));
+    }
+
+    @Test
+    /*
+     * Customer Test 9 
+     * Should return true when valid eircodes are entered.
+     */
+    public void testValidateEircode001(){
+        assertEquals(false, testCust.validateEircode("N37N6P6"));
+    }
+
+    @Test
+    /*
+     * Customer Test 10
+     * Should return false when passed in strings are shorter than seven characters
+     */
+    public void testValidateEircode(){
+        assertEquals(false, testCust.validateEircode("D01HJ1"));
     }
 
     @SuppressWarnings("deprecation")
@@ -95,7 +113,7 @@ public class CustomerTest extends Customer{
      * Should return true when a valid phone number is passed in.e
      */
     public void testValidatePhoneNo001(){
-        Assert.assertEquals(true, testCust.validatePhoneNo("0873925974"));
+        assertEquals(true, testCust.validatePhoneNo("0873925974"));
     }
 
 
