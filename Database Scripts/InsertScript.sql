eb889de099081dc077b4729b9e15f991f0a962cf
Dimitrije Sreckovic <dimitrijesreckovic02@gmail.com>
2024-10-21 12:01:25 +0100

    Add files via upload

---
+++ Database Scripts/InsertScript.sql
@@ -0,0 +1,9 @@
+insert into publications values(default, 'TheSun', null, 'Newspaper', 'Daily', 1.99);
+insert into publications values(default, 'EE', null, 'Magazine', 'Weekly', 3.50);
+insert into publications values(default, 'The Divine Comedy', 'Dante Alighieri', 'Book', 'Once', 1.99);
+
+insert into deliveryareas values(default, 'Monksland');
+insert into deliveryareas values(default, 'Garrycastle');
+
+insert into customers values(default, 'Dimitrije', 'Sreckovic', 0894622672, '138 River Village', 'N37Y6D5', 1);
+insert into customers values(default, 'John', 'Doe', 0891234567, '50 Cypress gardens', 'N37D5T7', 2);
