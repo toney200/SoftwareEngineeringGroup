3f85ae1a1bfb6a6c573691b4e3b8acb0ea85222c
toney200 <157587393+toney200@users.noreply.github.com>
2024-11-02 21:50:10 +0000

    Update MySQLConnector.java

--- Source/MySQLConnector.java
+++ Source/MySQLConnector.java
@@ -124,6 +124,35 @@ public class MySQLConnector {
 	
 		return insertSucessfull;
 	}
+
+	public ArrayList<Publication> searchPublication(String title){
+		ArrayList<Publication> publication = new ArrayList<Publication>();
+		boolean searchSuccessfull = true;
+
+		try{
+			preparedStatement = connect.prepareStatement("select * from publications where title like ?");
+			preparedStatement.setString(1, "%"+title+"%");
+			resultSet = preparedStatement.executeQuery();
+			// Create a new Publication object using data from the current row of the resultSet
+			while(resultSet.next()) {
+				publication.add(new Publication(
+						resultSet.getInt(1),
+						resultSet.getString(2),
+						resultSet.getString(3),
+						resultSet.getString(4),
+						resultSet.getString(5),
+						resultSet.getDouble(6)));
+			}
+
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			searchSuccessfull = false;
+		}
+
+		return publication;
+
+	}
 	
 //	public boolean insertOrderDetails(Order p) {
 //		
@@ -147,13 +176,13 @@ public class MySQLConnector {
 //		return insertSucessfull;
 //	}
 
-	// public static void main(String[] args) {
-	// 	try {
-	// 		MySQLConnector connector = new MySQLConnector();
-	// 	} catch(Exception e) {
-	// 		e.printStackTrace();
-	// 	}
+	 public static void main(String[] args) {
+	 	try {
+	 		MySQLConnector connector = new MySQLConnector();
+	 	} catch(Exception e) {
+	 		e.printStackTrace();
+	 	}
 		
-	// }
+	 }
 
 }
